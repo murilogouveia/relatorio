69fe4f42f83a7ed0b5698524179d7c844263f602
Murilo Gouveia <murilo_gouveia@outlook.com>
2019-11-17 01:04:08 -0300

    database linux

---
+++ Migrations/20191117040258_onLinux.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace relatorio.Migrations
+{
+    public partial class onLinux : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
